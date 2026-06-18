29899f45d1fd9c174f63c4220878e49e1f2b4e19
chokyuho <ju0468@gmail.com>
2026-06-18 09:37:54 +0900

    first commit

---
+++ src/test/java/com/example/spendolive/SpendoliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spendolive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendoliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
