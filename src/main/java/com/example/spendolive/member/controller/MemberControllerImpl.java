3db2400d9fb605437be9f6f1748f16e0f52a373c
joosuye0n <hamsuchuri@gmail.com>
2026-06-24 15:50:52 +0900

    공지사항/알림센터 ui수정중

--- src/main/java/com/example/spendolive/member/controller/MemberControllerImpl.java
+++ src/main/java/com/example/spendolive/member/controller/MemberControllerImpl.java
@@ -44,6 +44,7 @@ public class MemberControllerImpl implements MemberController{
             HttpSession session = request.getSession();
             session.setAttribute("isLogOn", true);
             session.setAttribute("memberInfo", memberVO);
+          
             
             mav.setViewName("redirect:/spendolive/main.do");  
         }
@@ -78,6 +79,7 @@ public class MemberControllerImpl implements MemberController{
         session.setAttribute("isLogOn", false);
         session.removeAttribute("memberInfo");
         session.removeAttribute("login_type");
+        session.removeAttribute("loginId");
         mav.setViewName("redirect:/spendolive/main.do");
         return mav;
     }
--- src/main/java/com/example/spendolive/notice/controller/NoticeController.java
+++ src/main/java/com/example/spendolive/notice/controller/NoticeController.java
@@ -1,95 +1,65 @@
 package com.example.spendolive.notice.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.spendolive.notice.service.NoticeService;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.spendolive.alert.service.AlertService;
-
-import java.util.List;
-import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.spendolive.notice.domain.NoticeDTO;
+import com.example.spendolive.notice.service.NoticeService;
 
 @Controller
 @RequestMapping("/spendolive/notice")
 public class NoticeController {
 
     private final NoticeService noticeService;
-private final AlertService alertService;
 
-    public NoticeController(NoticeService noticeService, AlertService alertService) {
+    public NoticeController(NoticeService noticeService) {
         this.noticeService = noticeService;
-        this.alertService = alertService;
     }
 
     @GetMapping("/center.do")
     public ModelAndView noticeCenter(
-        @RequestParam(value = "tab", required = false, defaultValue = "notice") String tab) {
-     
+            @RequestParam(value = "tab", required = false, defaultValue = "notice") String tab) {
 
         ModelAndView mav = new ModelAndView();
 
         mav.setViewName("common/layout");
-
-        mav.addObject("body_page",
-                "/WEB-INF/views/notice/noticeCenter.jsp");
-
+        mav.addObject("body_page", "/WEB-INF/views/notice/noticeCenter.jsp");
         mav.addObject("tab", tab);
 
-        if ("important".equals(tab)) {
-                mav.addObject("noticeList", noticeService.getImportantList());
-        } else {
-                mav.addObject("noticeList", noticeService.getNoticeList());
-        }
-
-        if ("unread".equals(tab)) {
-                mav.addObject("alertList", alertService.getUnreadList());
-        } else {
-                mav.addObject("alertList", 
-                alertService.getAlertList());
-        }
-
-
-        mav.addObject("noticeCount",
-                noticeService.getNoticeCount());
-
-        mav.addObject("importantCount",
-                noticeService.getPinnedCount());
-
-        mav.addObject("unreadCount", 
-                alertService.getUnreadList().size());
+        mav.addObject("noticeList", noticeService.getNoticeList());
+        mav.addObject("noticeCount", noticeService.getNoticeCount());
+        mav.addObject("importantCount", noticeService.getPinnedCount());
 
         return mav;
-        
     }
 
-
-        @GetMapping("/detail.do")
-        public ModelAndView noticeDetail(@RequestParam("noticeId") int noticeId) {
+    @GetMapping("/detail.do")
+    public ModelAndView noticeDetail(@RequestParam("noticeId") int noticeId) {
 
         ModelAndView mav = new ModelAndView();
 
         mav.setViewName("common/layout");
         mav.addObject("body_page", "/WEB-INF/views/notice/noticeDetail.jsp");
-
         mav.addObject("notice", noticeService.getNoticeDetail(noticeId));
 
         return mav;
-        }
+    }
 
-        @GetMapping("/ajax/noticeList.do")
-        @ResponseBody
-        public List<NoticeDTO> ajaxNoticeList() {
+    @GetMapping("/ajax/noticeList.do")
+    @ResponseBody
+    public List<NoticeDTO> ajaxNoticeList() {
         return noticeService.getNoticeList();
-        }
+    }
 
-        @GetMapping("/ajax/importantList.do")
-        @ResponseBody
-        public List<NoticeDTO> ajaxImportantList() {
+    @GetMapping("/ajax/importantList.do")
+    @ResponseBody
+    public List<NoticeDTO> ajaxImportantList() {
         return noticeService.getImportantList();
-        }
+    }
 }
