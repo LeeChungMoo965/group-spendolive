507227d5ea983466819c6d9adfc45eb5e33028ee
LeeChungMoo965 <cndan021@naver.com>
2026-07-15 18:53:25 +0900

    member model 리펙토링 최신화

--- src/main/java/com/example/spendolive/member/controller/MemberController.java
+++ src/main/java/com/example/spendolive/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.spendolive.member.domain.MemberVO;
 public interface MemberController {
@@ -16,8 +17,8 @@ public interface MemberController {
 	public ModelAndView login(@RequestParam Map<String, String> loginMap,HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView loginForm(@RequestParam String log, HttpServletRequest request, HttpServletResponse response) throws Exception;
-	public ResponseEntity  addMember(@ModelAttribute("member") MemberVO member,
-            HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView  addMember(@ModelAttribute("member") MemberVO member,
+            HttpServletRequest request, HttpServletResponse response,RedirectAttributes redirectAttributes) throws Exception;
 	public ModelAndView memberForm(HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public String sendSms(@RequestParam("phone") String phone, HttpServletRequest request) throws Exception;
 	public boolean verifySms(@RequestParam("inputCode") String inputCode, HttpServletRequest request) throws Exception;
@@ -25,7 +26,7 @@ public interface MemberController {
 	public boolean checkId(@RequestParam("id") String id) throws Exception;
 	public boolean checkEmail(@RequestParam("email") String email) throws Exception;
 	public boolean checkPhone(@RequestParam("phone") String phone) throws Exception;
-	public ModelAndView kakaoCallback(@RequestParam(value = "code", required = false) String code, HttpServletRequest request) throws Exception;
-	public ResponseEntity openBankingCallback(@RequestParam("code") String code,@RequestParam("state") String state, HttpServletRequest request, HttpServletResponse response ,HttpSession session) throws Exception;
+	public ModelAndView kakaoCallback(@RequestParam(value = "code", required = false) String code, HttpServletRequest request,RedirectAttributes redirectAttributes) throws Exception;
+	public ModelAndView openBankingCallback(@RequestParam("code") String code,@RequestParam("state") String state, HttpServletRequest request, HttpServletResponse response ,HttpSession session,RedirectAttributes redirectAttributes) throws Exception;
 	public String openBankingAuth() throws Exception;
 }
