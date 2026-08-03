d0632a7d5a97458ce18950d3b2a1e8c7e9303b14
joosuye0n <hamsuchuri@gmail.com>
2026-08-03 12:09:47 +0900

    캘린더 컨트롤러 따로 빼줌

--- src/main/java/com/example/spendolive/controller/SpendOliveController.java
+++ src/main/java/com/example/spendolive/controller/SpendOliveController.java
@@ -66,11 +66,6 @@ public class SpendOliveController {
         return mav;
     }
 
-    @RequestMapping(value = "/calendar.do", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView calendar(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return layout("/WEB-INF/views/calendar/calendar.jsp");
-    }
-
     @RequestMapping(value = "/expense.do", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView expense(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView mav = new ModelAndView();
