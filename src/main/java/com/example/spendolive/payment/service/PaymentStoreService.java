c91a840ad2145fa28a957562a0d91f10103c43ee
이충무 <cndan021@naver.com>
2026-09-24 05:31:37 +0900

    Add package declaration to PaymentStoreService.java

--- src/main/java/com/example/spendolive/payment/service/PaymentStoreService.java
+++ src/main/java/com/example/spendolive/payment/service/PaymentStoreService.java
@@ -1,3 +1,5 @@
+package com.example.spendolive.payment.service;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
